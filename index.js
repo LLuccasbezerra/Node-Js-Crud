49f56a5e0a807eb3075f4546f2a2317244c4b7d4
Luccas Bezerra <luccasifes@gmail.com>
2022-10-15 23:22:47 +0100

    home nav bar

--- index.js
+++ index.js
@@ -41,7 +41,9 @@ app.use("/", articlesController);
 app.use("/",categoriesController);
 
 app.get("/", (req, res) => {
-  res.render("index");
+    Article.findAll().then(articles => {
+      res.render("index",{articles: articles});
+    })
 });
 
 app.listen(8080, () => {
