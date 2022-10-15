12986007c6528893886d0aca98b79adceb2284da
Luccas Bezerra <luccasifes@gmail.com>
2022-10-16 00:52:50 +0100

    homepage update

--- index.js
+++ index.js
@@ -33,17 +33,36 @@ connection
     console.log(error);
   });
 
-app.use("/",categoriesController);
+app.use("/", categoriesController);
 app.use("/", articlesController);
 
-
-
-app.use("/",categoriesController);
+app.use("/", categoriesController);
 
 app.get("/", (req, res) => {
-    Article.findAll().then(articles => {
-      res.render("index",{articles: articles});
+  Article.findAll({
+    order: [["id", "DESC"]],
+  }).then((articles) => {
+    res.render("index", { articles: articles });
+  });
+});
+
+app.get("/:slug", (req, res) => {
+  var slug = req.params.slug;
+  Article.findOne({
+    where: {
+      slug: slug,
+    },
+  })
+    .then((article) => {
+      if (article != undefined) {
+        res.render("article", { article: article });
+      } else {
+        res.redirect("/");
+      }
     })
+    .catch((err) => {
+      res.redirect("/");
+    });
 });
 
 app.listen(8080, () => {
