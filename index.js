4957a94f1a14169df78a64bc89af3527378b0a80
Luccas Bezerra <luccasifes@gmail.com>
2022-10-17 14:59:31 +0100

    User controller and database

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const connection = require("./database/database");
 
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/ArticlesController");
+const usersController = require("./users/UsersController");
+
 
 const Article = require("./articles/Article");
 
 const Category = require("./categories/Category");
+
+const User = require("./users/User");
 
 //View engine
 app.set("view engine", "ejs");
@@ -35,6 +39,7 @@ connection
 
 app.use("/", categoriesController);
 app.use("/", articlesController);
+app.use("/", usersController );
 
 app.use("/", categoriesController);
 
